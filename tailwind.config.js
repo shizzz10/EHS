09c4b160880d43090cfdbfc52169bad964f45463
Sheraz Khan <skwtanai5@gmail.com>
2025-01-10 04:14:19 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",      // Specifically match index.html in the root directory
+    "./src/**/*.{html,js}" // Match HTML and JS files in the src folder and its subfolders
+  ],
+  theme: {
+   extend: {},
+  },
+  plugins: [],
+}
+
