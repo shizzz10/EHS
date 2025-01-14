4b9bfac79cbee3d607b8f85e89ff4fc549d832d1
Sheraz Khan <skwtanai5@gmail.com>
2025-01-15 01:11:20 +0500

    worked on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",      // Specifically match index.html in the root directory
+    "./index.html",         // Specifically match index.html in the root directory
     "./src/**/*.{html,js}" // Match HTML and JS files in the src folder and its subfolders
   ],
   theme: {
